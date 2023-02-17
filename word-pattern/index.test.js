2cea21292923b4dba106baa962ebc1509053a7ae
Denzil Doyle <denzildoyle@gmail.com>
2023-02-17 13:45:31 -0400

    Update unit test format

--- word-pattern/index.test.js
+++ word-pattern/index.test.js
@@ -2,19 +2,19 @@ const wordPattern = require('./index');
 
 describe("Word Pattern", () => {
 
-    test('Input: pattern = "abba", s = "dog cat cat dog"\nOutput: true', () => {
+    test('Input: pattern = "abba", s = "dog cat cat dog"\n      Output: true', () => {
         const pattern = "abba";
         const s = "dog cat cat dog";
         expect(wordPattern(pattern, s)).toBe(true);
     });
 
-    test('Input: pattern = "abba", s = "dog cat cat fish"\n Output: false', () => {
+    test('Input: pattern = "abba", s = "dog cat cat fish"\n     Output: false', () => {
         const pattern = "abba";
         const s = "dog cat cat fish";
         expect(wordPattern(pattern, s)).toBe(false);
     });
 
-    test('Input: pattern = "aaaa", s = "dog cat cat dog"\nOutput: false', () => {
+    test('Input: pattern = "aaaa", s = "dog cat cat dog"\n      Output: false', () => {
         const pattern = "aaaa";
         const s = "dog cat cat dog";
         expect(wordPattern(pattern, s)).toBe(false);
